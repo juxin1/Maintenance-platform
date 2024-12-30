a0308e3176c2023fcf09622053febf65e7ea04dd
JuXin <zhangf0719@163.com>
2024-12-30 13:28:04 +0800

    创建

---
+++ src/main/java/edu/tute/MaintenancePlatformApplication.java
@@ -0,0 +1,13 @@
+package edu.tute;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MaintenancePlatformApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MaintenancePlatformApplication.class, args);
+    }
+
+}
---
+++ src/test/java/edu/tute/MaintenancePlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.tute;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MaintenancePlatformApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
