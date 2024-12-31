c8c3eee708e8a1ce4a5a00f518d23a6c8ca081e9
JuXin <zhangf0719@163.com>
2024-12-31 08:51:21 +0800

    配置多端

---
+++ src/main/java/edu/tute/config/CorsConfig.java
@@ -0,0 +1,23 @@
+package edu.tute.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig {
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("http://localhost:3000/**")
+                        .allowedOrigins("http://localhost:3000")  // 前端开发服务器地址
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedHeaders("*")
+                        .allowCredentials(true);
+            }
+        };
+    }
+}
--- src/main/java/edu/tute/service/RepairService.java
+++
@@ -1,9 +0,0 @@
-package edu.tute.service;
-
-public interface RepairService {
-
-    /*
-    * 维修任务抢单(维修师)
-    * */
-    void repairAssignment(Integer orderId);
-}
